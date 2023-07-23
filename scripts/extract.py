fa0ca4084e7a74c4ba1faf01fbff0b870c846447
EF <efraisma.ef7@gmail.com>
2023-07-23 15:02:57 -0600

    Update extract.py
    load_sounds function finished :+1:

--- scripts/extract.py
+++ scripts/extract.py
@@ -3,19 +3,19 @@ import numpy as np
 from pathlib import Path
 
 
-def load_file_data(files_path: str, duration: int = 10, sr: int = 22050):
-    files_path = Path(files_path)
+def load_sounds(soundfiles_path: str, duration: int = 10, sr: int = 22050):
+    soundfiles_path = Path(soundfiles_path)
     input_length = sr * duration
 
     data = []
-    for sound_file in files_path.glob("*.wav"):
+    for sound_file in soundfiles_path.glob("*.wav"):
         # use kaiser_fast technique for faster extraction
         X, sr = librosa.load(
             str(sound_file), sr=sr, duration=duration, res_type="kaiser_fast"
         )
-        dur = librosa.get_duration(y=X, sr=sr)
 
         # pad audio file same duration
+        dur = librosa.get_duration(y=X, sr=sr)
         if round(dur) < duration:
             X = librosa.util.fix_length(data=X, size=input_length)
 
@@ -26,6 +26,3 @@ def load_file_data(files_path: str, duration: int = 10, sr: int = 22050):
         feature = np.array(mfccs).reshape([-1, 1])
         data.append(feature)
     return data
-
-a = load_file_data("data/heart_sound/val/unhealthy")
-print(a)

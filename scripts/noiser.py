15cfd2338f588f0409871aff651d5227d7d4ec3c
EF <efraisma.ef7@gmail.com>
2023-07-30 13:25:12 -0600

    noiser function and ignore mp3 files

--- scripts/noiser.py
+++ scripts/noiser.py
@@ -1,13 +1,20 @@
 from pydub import AudioSegment
-sound1 = AudioSegment.from_file("1.wav", format="wav")
-sound2 = AudioSegment.from_file("2.wav", format="wav")
-
-# sound1 6 dB louder
-louder = sound1 + 6
-
-# Overlay sound2 over sound1 at position 0  (use louder instead of sound1 to use the louder version)
-overlay = sound1.overlay(sound2, position=0)
 
 
-# simple export
-file_handle = overlay.export("output.mp3", format="mp3")
+def noiser(audio_dir: str, noise_dir: str, audio_louder: int) -> AudioSegment:
+    audio = AudioSegment.from_file(audio_dir, format="wav")
+    noise = AudioSegment.from_file(noise_dir, format="wav")
+
+    audio += audio_louder
+    mixed = audio.overlay(noise, position=0)
+
+    return mixed
+
+
+mixed = noiser(
+    audio_dir="data/heart_sound/train/healthy/a0007.wav",
+    noise_dir="data/hospital_noise/seg_1.wav",
+    audio_louder=0,
+)
+
+mixed.export("output.mp3", format="mp3")

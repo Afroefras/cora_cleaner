f59818935b9787e49a917d2457239a1f1f226911
EF <efraisma.ef7@gmail.com>
2023-07-30 14:20:44 -0600

    Update noiser.py
    Add k-noises to each audio and keep track of it

--- scripts/noiser.py
+++ scripts/noiser.py
@@ -1,20 +1,46 @@
+from pathlib import Path
 from pydub import AudioSegment
+from random import choices, randint
 
 
-def noiser(audio_dir: str, noise_dir: str, audio_louder: int) -> AudioSegment:
-    audio = AudioSegment.from_file(audio_dir, format="wav")
-    noise = AudioSegment.from_file(noise_dir, format="wav")
+def noiser(audio_path: str, noise_path: str, louder: int) -> AudioSegment:
+    audio = AudioSegment.from_file(audio_path, format="wav")
+    noise = AudioSegment.from_file(noise_path, format="wav")
 
     audio += audio_louder
     mixed = audio.overlay(noise, position=0)
 
-    return mixed
+    return noised
 
 
-mixed = noiser(
-    audio_dir="data/heart_sound/train/healthy/a0007.wav",
-    noise_dir="data/hospital_noise/seg_1.wav",
-    audio_louder=0,
-)
+def noise_dir(audios_dir: str, noises_dir: str, output_dir: str, k_noises: int) -> None:
+    output_dir = Path(output_dir)
+    output_dir.mkdir(exist_ok=True)
 
-mixed.export("output.mp3", format="mp3")
+    noises = Path(noises_dir).glob("**/*.wav")
+    noises = list(noises)
+
+    for audio in Path(audios_dir).glob("**/*.wav"):
+        audio = str(audio)
+        choosed_noises = choices(noises, k=k_noises)
+
+        for noise in choosed_noises:
+            db_louder = randint(17, 25)
+            noised = noiser(audio, noise, louder=db_louder)
+
+            new_audio_name = audio.replace(audios_dir, "")
+            new_audio_name = new_audio_name.replace("/", "_")
+            new_audio_name = new_audio_name.replace(".wav", "")
+
+            noise_name = noise.stem
+            new_audio_name += f"_{db_louder}dB_{noise_name}noise.mp3"
+
+            noised.export(output_dir.joinpath(new_audio_name), format="mp3")
+
+
+# noise_dir(
+#     audios_dir="data/heart_sound",
+#     noises_dir="data/hospital_noise",
+#     output_dir="data/heart_noised",
+#     k_noises=5
+# )

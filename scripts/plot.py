f79641503ae41b73356ebdd3542c4cc29c4c0207
EF <efraisma.ef7@gmail.com>
2023-08-09 16:48:06 -0600

    plot uses torch

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,20 +1,20 @@
-import matplotlib.pyplot as plt
-import librosa.display
 import numpy as np
-
+import librosa.display
+from torch import Tensor
+import matplotlib.pyplot as plt
 
 def plot_audio_sample(audio_data, title):
     """
     Grafica un registro de audio de tamaño 25x1, su espectrograma y su MFCC.
 
     Args:
-        audio_data (numpy.ndarray): Datos de audio de tamaño 25x1.
+        audio_data (torch.Tensor): Datos de audio de tamaño 25x1.
         title (str): Título para el gráfico.
     """
     plt.figure(figsize=(12, 7))
 
     # Convertir los datos de audio a formato de punto flotante
-    audio_data_float = audio_data.astype(np.float32)
+    audio_data_float = Tensor(audio_data).float()
 
     # Gráfico de la forma de onda
     plt.subplot(2, 2, 1)
@@ -27,7 +27,7 @@ def plot_audio_sample(audio_data, title):
     # Espectrograma
     plt.subplot(2, 2, 2)
     spectrogram = librosa.amplitude_to_db(
-        np.abs(librosa.stft(audio_data_float.squeeze())), ref=np.max
+        np.abs(librosa.stft(audio_data_float.squeeze().numpy())), ref=np.max
     )
     librosa.display.specshow(spectrogram, sr=22050, x_axis="time", y_axis="log")
     plt.colorbar(format="%+2.0f dB")
@@ -35,7 +35,9 @@ def plot_audio_sample(audio_data, title):
 
     # MFCC
     plt.subplot(2, 1, 2)
-    mfcc = librosa.feature.mfcc(y=audio_data_float.squeeze(), sr=22050)
+    mfcc = librosa.feature.mfcc(
+        y=audio_data_float.squeeze().numpy(), sr=22050
+    )
     librosa.display.specshow(mfcc, x_axis="time")
     plt.colorbar()
     plt.title("MFCC - " + title)
